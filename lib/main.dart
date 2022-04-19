28cca70073d2df5aa215c02252e68ccfa93043be
Gautam Rana <gautamrana023@gmail.com>
2022-04-19 10:59:46 +1000

    firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
